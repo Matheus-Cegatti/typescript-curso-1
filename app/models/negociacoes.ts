6251771cc17f6e40b792ea2510b993274b959a1a
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-05 15:32:21 -0300

    simplificando o array

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
-
+    // private negociacoes: Array<Negociacao> = []; //existe as duar formas de declarar o array, essa e a da linha abaixo
+    private negociacoes: Negociacao[] = [];
     adiciona(negociacao :Negociacao) {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao>  {
+    // lista(): ReadonlyArray<Negociacao>  { //existe as duar formas de colocar readonly no array ( para não permitir modificaçoes no array como o "pop" por exemplo), essa e a da linha abaixo
+        lista(): readonly Negociacao[]  {
         return this.negociacoes
     }
 }
